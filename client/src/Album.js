30ed424dd18751a3af6b2d258db83f21a75b3909
Saqif Abrar <abrars@uoguelph.ca>
2023-11-27 09:51:24 -0500

    Added MUI accordians to prequisite-setups and each tool

--- client/src/Album.js
+++ client/src/Album.js
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { useState } from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -10,9 +13,10 @@ import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-//	import Link from "@mui/material/Link";
+import Link from "@mui/material/Link";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Tool1 from "./Tool1";
 import Tool2 from "./Tool2";
@@ -33,48 +37,58 @@ function ProjectDetailsUnit() {
 			}}
 		>
 			<Container maxWidth="sm">
-				<Typography
-					component="p"
-					variant="h7"
-					align="center"
-					color="text.primary"
-					gutterBottom
-				>
-					<h2>Project Details</h2>
-					This website has various elements of interaction. You'll find that
-					there will be tests on the actual website itself. Below, you'll see a
-					3 cards for each JavaScript tool and a standard sign-in page commonly
-					found on social media and e-commerce websites. Click to discover each
-					tool and how they test different types of testing methods to learn
-					more.
-					<br />
-					<br />
-					The project consists of the frontend (client) and the backend
-					(server). The client is a react development folder with all the basic
-					components of the frontend. It is compiled into a static servable
-					website through the node.js server.
-					<br />
-					<h3>The Frontend (Client)</h3>
-					The frontend. The React.js frontend (found in /client) consists of
-					App.js, Album.js and SignIn.js. App.js - Contains all the components
-					of this website. Album.js - Contains all the tutorial information and
-					testing instructions/results for this module. SignIn.js - Contains the
-					practical use case code for testing (note that Album.js is also used
-					for testing as well).
-					<br />
-					<h3>The Backend (Server)</h3>
-					The backend. The Node.js backend (found in /server) consists of
-					./cypress, ./jest, ./model (database) and index.js. Note that SQLlite
-					is being used to store user information. There are several endpoints
-					in the backend and some validation functions that have great potential
-					to be tested!{" "}
-					<b>
-						<u>
-							In order to use the backend, you must use a local copy of this
-							project (found in the github repo).
-						</u>
-					</b>
-				</Typography>
+				<h2>Project Details</h2>
+				This website has various elements of interaction. You'll find that there
+				will be tests on the actual website itself. Below, you'll see a 3 cards
+				for each JavaScript tool and a standard sign-in page commonly found on
+				social media and e-commerce websites. Click to discover each tool and
+				how they test different types of testing methods to learn more.
+				<br />
+				<br />
+				The project consists of the frontend (client) and the backend (server).
+				The client is a react development folder with all the basic components
+				of the frontend. It is compiled into a static servable website through
+				the node.js server.
+				<br />
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<h3>The Frontend (Client)</h3>
+					</AccordionSummary>
+					<AccordionDetails>
+						The frontend. The React.js frontend (found in /client) consists of
+						App.js, Album.js and SignIn.js. App.js - Contains all the components
+						of this website. Album.js - Contains all the tutorial information
+						and testing instructions/results for this module. SignIn.js -
+						Contains the practical use case code for testing (note that Album.js
+						is also used for testing as well).
+					</AccordionDetails>
+				</Accordion>
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<h3>The Backend (Server)</h3>
+					</AccordionSummary>
+					<AccordionDetails>
+						The backend. The Node.js backend (found in /server) consists of
+						./cypress, ./jest, ./model (database) and index.js. Note that
+						SQLlite is being used to store user information. There are several
+						endpoints in the backend and some validation functions that have
+						great potential to be tested!{" "}
+						<b>
+							<u>
+								In order to use the backend, you must use a local copy of this
+								project (found in the github repo).
+							</u>
+						</b>
+					</AccordionDetails>
+				</Accordion>
 			</Container>
 		</Box>
 	);
@@ -90,25 +104,74 @@ function SetUpDetailsUnit() {
 			}}
 		>
 			<Container maxWidth="sm">
-				<Typography
-					component="p"
-					variant="h7"
-					align="center"
-					color="text.primary"
-					gutterBottom
-				>
-					1. Install Node.js
-					<br />
-					<br />
-					2. Install React.js
-					<br />
-					<br />
-					3. commit sepuku because you have commited war crimes so you must
-					attone for your sins and protect your honour.
-					<br />
-					<br />
-					4. Run `npm install`
-				</Typography>
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						0. Pre-requisite set-up
+					</AccordionSummary>
+					<AccordionDetails>
+						a) Install the latest version of{" "}
+						<Link href="https://nodejs.org/en/download">Node.js</Link>
+						<br />
+						<br />
+						b) Install the latest version of{" "}
+						<Link href="https://www.sqlite.org/download.html">SQLite3</Link>
+						<br />
+					</AccordionDetails>
+				</Accordion>
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						1. Run client (frontend)
+					</AccordionSummary>
+					<AccordionDetails>
+						To run the client, go to the <code>./server</code> directory and run{" "}
+						<br />
+						<code>
+							<b>npm run start</b>
+						</code>
+					</AccordionDetails>
+				</Accordion>
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						2. Run server (backend)
+					</AccordionSummary>
+					<AccordionDetails>
+						To run the server, go to the <code>./server</code> directory and run{" "}
+						<br />
+						<code>
+							<b>npm run start</b>
+						</code>
+						<br />
+						<br />
+						<b>Note:</b> To update the <code>./server/build</code> directory
+						that serves the browser the user interface from the server, run{" "}
+						<br />
+						<code>
+							<b>npm run build</b>
+						</code>
+						<br />
+						in the <code>./client</code> directory
+					</AccordionDetails>
+				</Accordion>
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					></AccordionSummary>
+					<AccordionDetails></AccordionDetails>
+				</Accordion>
 			</Container>
 		</Box>
 	);
--- client/src/Tool2.js
+++ client/src/Tool2.js
@@ -1,85 +1,135 @@
+import { CodeBlock } from "react-code-blocks";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import { CodeBlock } from "react-code-blocks";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function Tool2() {
 	return (
 		<Box>
 			<Container>
 				<h1>Jest</h1>
-				<p>Jest is a JavaScript Testing Framework with a focus on simplicity. It works with projects using various tools  and has all you ned to write comprehensive tests. </p>
-				<h2>Installation Instructions</h2>
-				<p>To install Jest, use the following npm command:</p>
-				<CodeBlock
-				text={`npm install jest --save-dev`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
-				<h2>Basics of Jest Testing</h2>
 				<p>
-				Some of the Basic jest Function Include:
+					Jest is a JavaScript Testing Framework with a focus on simplicity. It
+					works with projects using various tools and has all you ned to write
+					comprehensive tests.{" "}
 				</p>
 
-				<ul>
-				<li><strong>describe:</strong> Creates a block that groups together several related tests.</li>
-				<li><strong>test:</strong> Used for writing individual tests.</li>
-				<li><strong>expect:</strong> Lets you access a number of “matchers” that let you validate different things.</li>
-				</ul> 
-				
-				
-				<p>
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<h2>Installation Instructions</h2>
+					</AccordionSummary>
+					<AccordionDetails>
+						<p>To install Jest, use the following npm command:</p>
+						<CodeBlock
+							text={`npm install jest --save-dev`}
+							language="bash"
+							theme="duotoneLight"
+						/>
+					</AccordionDetails>
+				</Accordion>
 
-				Applying these functions you can create tests to ensure proper functioning of an application
-				</p>
- 				<p>Example of a simple assertion in Jest:</p>
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<h2>Basics of Jest Testing</h2>
+					</AccordionSummary>
+					<AccordionDetails>
+						<p>Some of the Basic jest Function Include:</p>
 
-				<CodeBlock
-				text={`test('Add 1 + 1', () => {
+						<ul>
+							<li>
+								<strong>describe:</strong> Creates a block that groups together
+								several related tests.
+							</li>
+							<li>
+								<strong>test:</strong> Used for writing individual tests.
+							</li>
+							<li>
+								<strong>expect:</strong> Lets you access a number of “matchers”
+								that let you validate different things.
+							</li>
+						</ul>
+
+						<p>
+							Applying these functions you can create tests to ensure proper
+							functioning of an application
+						</p>
+						<p>Example of a simple assertion in Jest:</p>
+
+						<CodeBlock
+							text={`test('Add 1 + 1', () => {
 const result = 1 + 1;
 expect(result).toBe(2);
 });`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
+							language="bash"
+							theme="duotoneLight"
+						/>
 
-				<p>
-				This test checks for the result of a simple addition operation, ensuring that it equals 2.
-				<p/>
-				<h2>Overview of different assertions</h2>
+						<p>
+							This test checks for the result of a simple addition operation,
+							ensuring that it equals 2.{" "}
+						</p>
+					</AccordionDetails>
+				</Accordion>
 
-				<p>
-				Here’s an overview of some common "matchers" in Jest:
-				</p>
-				<h3>Equality and Inequality</h3>
-				
-				<CodeBlock
-				text={`expect(a).toBe(b);    // Asserts that a is equal to b
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<h2>Overview of different assertions</h2>
+					</AccordionSummary>
+					<AccordionDetails>
+						<p>Here’s an overview of some common "matchers" in Jest:</p>
+						<h3>Equality and Inequality</h3>
+						<CodeBlock
+							text={`expect(a).toBe(b);    // Asserts that a is equal to b
 expect(a).not.toBe(b);    // Asserts that a is not equal to b`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
-								<h3>Containment</h3>
-				
-				<CodeBlock
-				text={`expect(sequence).toContain(item);    // Asserts that item is in the sequence
+							language="bash"
+							theme="duotoneLight"
+						/>
+						<h3>Containment</h3>
+						<CodeBlock
+							text={`expect(sequence).toContain(item);    // Asserts that item is in the sequence
 expect(sequence).not.toContain(item); // Asserts that item is not in the sequence`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
-								<h3>Greater and Less Than</h3>
-				
-				<CodeBlock
-				text={`expect(a).toBeGreaterThan(b);   // Asserts that a is greater than b
+							language="bash"
+							theme="duotoneLight"
+						/>
+						<h3>Greater and Less Than</h3>
+						<CodeBlock
+							text={`expect(a).toBeGreaterThan(b);   // Asserts that a is greater than b
 expect(a).toBeLessThan(b);   // Asserts that a is less than b`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
+							language="bash"
+							theme="duotoneLight"
+						/>
+					</AccordionDetails>
+				</Accordion>
 
-				<h2>Testing Asynchronous Code</h2>
-				To test Asynchronous Code Simply put the async keyword in the testfunction  and make sure to await before calling the asynchronus function
-
-				<CodeBlock
-				text={`// Synchronus Test
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<h2>Testing Asynchronous Code</h2>
+					</AccordionSummary>
+					<AccordionDetails>
+						To test Asynchronous Code Simply put the async keyword in the
+						testfunction and make sure to await before calling the asynchronus
+						function
+						<CodeBlock
+							text={`// Synchronus Test
 test('adds 1 + 2 to equal 3', () => {
 	expect(add(1, 2)).toBe(3);
 });
@@ -90,30 +140,40 @@ test('adds 1 + 2 to equal 3', async () => {
   const result = await asyncAdd(1, 2);
   expect(result).toBe(3);
 });`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
+							language="bash"
+							theme="duotoneLight"
+						/>
+					</AccordionDetails>
+				</Accordion>
 
-
-
-
-
-
-				</p>
-				<h2>Running Tests</h2>
-				<p>To run your Jest tests, you run the following command</p>
-				<CodeBlock
-				text={`npx jest`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
-				<p>This will run all test files with the .test.js extension</p>
-				<h2>Test & Tutorial Discussions</h2>
-				<p>
-					Talk about the tests you made in greater detail. Talk about how the
-					test cases are structured, how the tools is used, and how effective
-					are they.
-				</p>
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<h2>Running Tests</h2>
+					</AccordionSummary>
+					<AccordionDetails>
+						<>
+							<p>To run your Jest tests, you run the following command</p>
+							<CodeBlock
+								text={`npx jest`}
+								language="bash"
+								theme="duotoneLight"
+							/>
+							<p>This will run all test files with the .test.js extension</p>
+						</>
+						<>
+							<h2>Test & Tutorial Discussions</h2>
+							<p>
+								Talk about the tests you made in greater detail. Talk about how
+								the test cases are structured, how the tools is used, and how
+								effective are they.
+							</p>
+						</>
+					</AccordionDetails>
+				</Accordion>
 			</Container>
 		</Box>
 	);
