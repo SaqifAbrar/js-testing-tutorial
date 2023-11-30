ab37c645592fbfc12d4875a68768e254138f0942
Saqif Abrar <abrars@uoguelph.ca>
2023-11-29 23:25:46 -0500

    Finalized version

--- client/src/Tool2.js
+++ client/src/Tool2.js
@@ -26,31 +26,52 @@ export default function Tool2() {
 						<h2>Jest Evaluation</h2>
 					</AccordionSummary>
 					<AccordionDetails>
-					
-				
-				<h3> Accuracy</h3>
-				<p> Jest scores highly in its accuracy at testing Javascript code. It provides clear and specific error messages when tests fail making it easier to identify issues. It alose supports snapshot testing to identify unexpected changes in code's behaviour</p>
+						<h3> Accuracy</h3>
+						<p>
+							{" "}
+							Jest scores highly in its accuracy at testing Javascript code. It
+							provides clear and specific error messages when tests fail making
+							it easier to identify issues. It alose supports snapshot testing
+							to identify unexpected changes in code's behaviour
+						</p>
 
-				<h3> Ease of Use</h3>
-				<p> Jest is an easy to use test platform. It works with minimal configuration for most Java script project and has command line support for running tests</p>
+						<h3> Ease of Use</h3>
+						<p>
+							{" "}
+							Jest is an easy to use test platform. It works with minimal
+							configuration for most Java script project and has command line
+							support for running tests
+						</p>
 
-				<h3> Reporting Facility</h3>
-				<p> provides detailed reports on tests reulsts on which tests failed and why those failures occured. It also has code coverage reports allowing you  to see which parts of your code are not covered by tests</p>
+						<h3> Reporting Facility</h3>
+						<p>
+							{" "}
+							provides detailed reports on tests reulsts on which tests failed
+							and why those failures occured. It also has code coverage reports
+							allowing you to see which parts of your code are not covered by
+							tests
+						</p>
 
-				<h3> Automation and Integration</h3>
-				<p> Jest supports automated tests which can be run in parrallel for maximum performance. It has built in support for mocking. Jest is compatimblle with other prohject like Babel, TypeScript, Node, React, Angular and Vue </p>
+						<h3> Automation and Integration</h3>
+						<p>
+							{" "}
+							Jest supports automated tests which can be run in parrallel for
+							maximum performance. It has built in support for mocking. Jest is
+							compatimblle with other prohject like Babel, TypeScript, Node,
+							React, Angular and Vue{" "}
+						</p>
 
-				<h3> Overall</h3>
-				<p> Jest is a comprehensive testing framework that excels in accuracy, ease of use, reporting, and automation. It is well-integrated, well-supported, and widely used in the JavaScript community.It should have the necesssary tools and geatures for you testing needs</p>
-
+						<h3> Overall</h3>
+						<p>
+							{" "}
+							Jest is a comprehensive testing framework that excels in accuracy,
+							ease of use, reporting, and automation. It is well-integrated,
+							well-supported, and widely used in the JavaScript community.It
+							should have the necesssary tools and geatures for you testing
+							needs
+						</p>
 					</AccordionDetails>
 				</Accordion>
-
-
-
-
-
-
 
 				<Accordion>
 					<AccordionSummary
@@ -149,13 +170,13 @@ expect(a).toBeLessThan(b);   // Asserts that a is less than b`}
 							theme="duotoneLight"
 						/>
 
-<h3>SnapShot Testing</h3>
-				
-				<CodeBlock
-				text={`expect(a).toMatchSnapshot();   // Asserts that a has not changed since the last time the test was run`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
+						<h3>SnapShot Testing</h3>
+
+						<CodeBlock
+							text={`expect(a).toMatchSnapshot();   // Asserts that a has not changed since the last time the test was run`}
+							language="bash"
+							theme="duotoneLight"
+						/>
 					</AccordionDetails>
 				</Accordion>
 
@@ -207,7 +228,6 @@ test('adds 1 + 2 to equal 3', async () => {
 							/>
 							<p>This will run all test files with the .test.js extension</p>
 						</>
-
 					</AccordionDetails>
 				</Accordion>
 
@@ -221,38 +241,41 @@ test('adds 1 + 2 to equal 3', async () => {
 					</AccordionSummary>
 					<AccordionDetails>
 						<>
-						<p>
-					As an Example to Create test cases to ensure a specific function is working  I will walk through how to Unit test the searchUser Function using the tools provided by jest.
-					<h3>Step 1: Begin the test suite</h3>
-				<CodeBlock
-				text={`const searchUser = require('./dbConfig.js');
+							<p>
+								As an Example to Create test cases to ensure a specific function
+								is working I will walk through how to Unit test the searchUser
+								Function using the tools provided by jest.
+								<h3>Step 1: Begin the test suite</h3>
+								<CodeBlock
+									text={`const searchUser = require('./dbConfig.js');
 
 describe('searchUser', () => {`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
-
-				<p>The first line imports the <strong>searchUser</strong> function from the dbConfig.js and the second uses the <strong>describe</strong> function to begin a test suite for the SearchUser function</p>
-
-					<h3>Step 2: Test Case Email is empty</h3>
-					<CodeBlock
-				text={`test('should return -1 when email is empty', async () => {
+									language="bash"
+									theme="duotoneLight"
+								/>
+								<p>
+									The first line imports the <strong>searchUser</strong>{" "}
+									function from the dbConfig.js and the second uses the{" "}
+									<strong>describe</strong> function to begin a test suite for
+									the SearchUser function
+								</p>
+								<h3>Step 2: Test Case Email is empty</h3>
+								<CodeBlock
+									text={`test('should return -1 when email is empty', async () => {
 	const result = await searchUser('', {});
 	expect(result).toBe(-1);
 });`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
-
-				<p>This test case checks if searchUser returns -1 when the email is an empty string. The expect function is used to check if the actual result matches the expected result.</p>
-
-
-
-
-					<h3>Step 3: Test Case User is not found</h3>
-
-					<CodeBlock
-				text={`test('should return -1 when user is not found', async () => {
+									language="bash"
+									theme="duotoneLight"
+								/>
+								<p>
+									This test case checks if searchUser returns -1 when the email
+									is an empty string. The expect function is used to check if
+									the actual result matches the expected result.
+								</p>
+								<h3>Step 3: Test Case User is not found</h3>
+								<CodeBlock
+									text={`test('should return -1 when user is not found', async () => {
 					const db = {
 					  all: jest.fn((query, params, callback) => callback(null, [])),
 					};
@@ -260,15 +283,18 @@ describe('searchUser', () => {`}
 					const result = await searchUser('notfound@example.com', db);
 					expect(result).toBe(-1);
 				  });`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
-
-				<p>This test case checks if searchUser returns -1 when the user is not found in the database. A mock database object is created with a jest.fn() function that simulates a scenario where the user is not found.</p>
-
-					<h3>Step 4: Test Case User is found</h3>
-					<CodeBlock
-				text={`test('should return user id when user is found', async () => {
+									language="bash"
+									theme="duotoneLight"
+								/>
+								<p>
+									This test case checks if searchUser returns -1 when the user
+									is not found in the database. A mock database object is
+									created with a jest.fn() function that simulates a scenario
+									where the user is not found.
+								</p>
+								<h3>Step 4: Test Case User is found</h3>
+								<CodeBlock
+									text={`test('should return user id when user is found', async () => {
 					const db = {
 					  all: jest.fn((query, params, callback) => callback(null, [{ id: 123 }])),
 					};
@@ -276,48 +302,52 @@ describe('searchUser', () => {`}
 					const result = await searchUser('found@example.com', db);
 					expect(result).toBe(123);
 				  });`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
-
-				<p>This test case checks if searchUser returns the correct user id when the user is found in the database. A mock database object is created with a jest.fn() function that simulates a scenario where the user is found.</p>
-
-				<h3>Step 5: Run the Tests</h3>
-				<p> Run the Test suite with </p>
-				<CodeBlock
-				text={`npx jest`}
-				language="bash"
-				theme="duotoneLight"
-      			/>
-				<p> If the test suite passes that means that the searchUser function performs as expected. If it does not pass all the test the specific test failures can help determine what functionality is missing from the search User function</p>
-				</p>
+									language="bash"
+									theme="duotoneLight"
+								/>
+								<p>
+									This test case checks if searchUser returns the correct user
+									id when the user is found in the database. A mock database
+									object is created with a jest.fn() function that simulates a
+									scenario where the user is found.
+								</p>
+								<h3>Step 5: Run the Tests</h3>
+								<p> Run the Test suite with </p>
+								<CodeBlock
+									text={`npx jest`}
+									language="bash"
+									theme="duotoneLight"
+								/>
+								<p>
+									{" "}
+									If the test suite passes that means that the searchUser
+									function performs as expected. If it does not pass all the
+									test the specific test failures can help determine what
+									functionality is missing from the search User function
+								</p>
+							</p>
 						</>
-
 					</AccordionDetails>
 				</Accordion>
 
-
-
-			<Accordion>
-				<AccordionSummary
-					expandIcon={<ExpandMoreIcon />}
-					aria-controls="panel1a-content"
-					id="panel1a-header"
-				>
-					<h2>Jtest Demo</h2>
-				</AccordionSummary>
-				<AccordionDetails>
-					<>
-					<p>
-						recording of using Jtest
-					</p>
-					<video controls width="100%">
-						<source src="/videos/jtest_proper.mp4" type="video/mp4" />
-						Your browser does not support the video tag.
-					</video>
-					</>
-				</AccordionDetails>
-			</Accordion>
+				<Accordion>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<h2>Jest Demo</h2>
+					</AccordionSummary>
+					<AccordionDetails>
+						<>
+							<p>recording of using Jest</p>
+							<video controls width="100%">
+								<source src="/videos/jtest_proper.mp4" type="video/mp4" />
+								Your browser does not support the video tag.
+							</video>
+						</>
+					</AccordionDetails>
+				</Accordion>
 			</Container>
 		</Box>
 	);
